1c2ad648b209a1b934111b642f47f4a7fbbb2e69
Andrew Louth <andrew.louth@sportsbet.com.au>
2019-05-10 11:13:09 +1000

    Comments out api calls

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -14,24 +14,24 @@ class Fib extends Component {
   }
 
   async fetchValues() {
-    const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+    // const values = await axios.get('/api/values/current');
+    // this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
-    this.setState({
-      seenIndexes: seenIndexes.data
-    });
+    // const seenIndexes = await axios.get('/api/values/all');
+    // this.setState({
+    //   seenIndexes: seenIndexes.data
+    // });
   }
 
   handleSubmit = async event => {
-    event.preventDefault();
-
-    await axios.post('/api/values', {
-      index: this.state.index
-    });
-    this.setState({ index: '' });
+    // event.preventDefault();
+    //
+    // await axios.post('/api/values', {
+    //   index: this.state.index
+    // });
+    // this.setState({ index: '' });
   };
 
   renderSeenIndexes() {
